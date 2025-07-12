c2c3d449463265a05d48d5519cb44dfe228af9cb
Dakshin Priya T S <tsdakshinpriya@gmail.com>
2025-07-12 10:46:43 +0530

    Create Factors Using Square Root Technique.c

---
+++ Factors Using Square Root Technique.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+void printFactors(int n) {
+    printf("Factors of %d are:\n", n);
+
+    // First print all smaller divisors and their pair
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+            if (i != n / i) {
+                printf("%d ", n / i); // print the paired factor only if it's different
+            }
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Please enter a positive number.\n");
+    } else {
+        printFactors(num);
+    }
+
+    return 0;
+}
